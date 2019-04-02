d309fe836f779bd6696919ff371824451dd96b5b
庄培龙 <peilong.zhuang@office.feng.com>
2019-04-02 17:35:33 +0800

    update

--- TestDemo.py
+++ TestDemo.py
@@ -1,5 +1,7 @@
 url = 'https://www.androeed.ru/files/mx-player-pro.html?hl=en'
-
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.96 Safari/537.36',
+}
 import requests
-r = requests.get(url=url)
+r = requests.get(url=url,headers=headers)
 print('stasu:'+str(r.status_code))
