832983a0a40e8c46679ca456a811f9c2e5366f22
庄培龙 <peilong.zhuang@office.feng.com>
2019-04-02 17:39:52 +0800

    update

--- TestDemo.py
+++ TestDemo.py
@@ -1,4 +1,4 @@
-url = 'https://www.androeed.ru/files/mx-player-pro.html?hl=en'
+url = 'https://www.androeed.ru/files/unkilled.html?hl=en'
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.96 Safari/537.36',
 }
