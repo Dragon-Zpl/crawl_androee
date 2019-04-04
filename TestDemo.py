a16db58f183e80b73f8c17cead16cb7296f61a0d
庄培龙 <peilong.zhuang@office.feng.com>
2019-04-04 10:41:24 +0800

    update

--- TestDemo.py
+++ TestDemo.py
@@ -81,12 +81,15 @@ def analysis_data(data):
                     # 第一个为apk包，第二个为破解包
                     temp_apk_download_url = mod_content.xpath(analysis.download_first_url)[-2]
                     temp_obb_download_url = mod_content.xpath(analysis.download_first_url)[-1]
-                    apk_download_url = temp_apk_download_url.xpath(analysis.pkg_download_url)[0]
-                    obb_download_url = temp_obb_download_url.xpath(analysis.pkg_download_url)[0]
+                    temp_apk_download_url_data = etree.HTML(requests.get(temp_apk_download_url).text)
+                    temp_obb_download_url_data = etree.HTML(requests.get(temp_obb_download_url).text)
+                    apk_download_url = temp_apk_download_url_data.xpath(analysis.pkg_download_url)[0]
+                    obb_download_url = temp_obb_download_url_data.xpath(analysis.pkg_download_url)[0]
                     data_dic["download_first_url"] = [apk_download_url,obb_download_url]
                 elif download_url_len == 4:
                     temp_apk_download_url = mod_content.xpath(analysis.download_first_url)[-2]
-                    apk_download_url = temp_apk_download_url.xpath(analysis.pkg_download_url)[0]
+                    temp_apk_download_url_data = etree.HTML(requests.get(temp_apk_download_url).text)
+                    apk_download_url = temp_apk_download_url_data.xpath(analysis.pkg_download_url)[0]
                     data_dic["download_first_url"] = [apk_download_url]
                 else:
                     data_dic["download_first_url"] = "None"
