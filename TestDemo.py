f53017df4377850d644c1ddbdd9d6a0cfdc4492a
庄培龙 <peilong.zhuang@office.feng.com>
2019-04-04 10:23:52 +0800

    update

--- TestDemo.py
+++ TestDemo.py
@@ -1,43 +1,97 @@
-# import hashlib
-# import os
-# import shutil
-#
-# from helper import Helper
-#
-#
-#
-#
-# def _download_obb(obbname, basic_dir, apk_details, pkgname):
-#     md5_pkgname = hashlib.md5((pkgname).encode('utf-8')).hexdigest()
-#     apk_path = basic_dir + md5_pkgname + ".apk"
-#     app_path = basic_dir + md5_pkgname + "/"
-#     obb_path_temp = app_path + obbname
-#     if os.path.exists(basic_dir + obbname):
-#         os.system("rm {}".format(basic_dir + obbname))
-#     shutil.move(obb_path_temp, basic_dir)
-#     obbpath = basic_dir + obbname
-#     tpkpath = _compose_tpk(apk_details, apk_path, obbpath, basic_dir, pkgname)
-#     os.system("rm -rf {}".format(app_path))
-#
-# def _compose_tpk(self, apk_details, apkpath, obbpath, basic_dir, pkgname):
-#     dict_tpk = Helper.configinfo(apk_details, apkpath)
-#     Helper.build_tpk(basic_dir, obbpath, pkgname, dict_tpk)
-#     md5_pkgname = hashlib.md5((pkgname).encode('utf-8')).hexdigest()
-#     tpkpath = basic_dir + md5_pkgname + ".tpk"
-#     return tpkpath
-
-
 import re
 
-a = """
-package: name='com.outerminds.tubular' versionCode='120' versionName='1.36.0' platformBuildVersionName='6.0-2438415'
-install-location:'preferExternal'
-sdkVersion:'16'
-targetSdkVersion:'27'
-application-label:'PewDiePie: Tuber Simulator'
-application-label-ca:'PewDiePie: Tuber Simulator'
-application-label-da:'PewDiePie: Tuber Simulator'
-application-label-fa:'PewDiePie: Tuber Simulator'
-"""
-pkgname = re.search(r'package: name=\'(.*?)\'', a)
-print(pkgname.group(1))
+import requests
+
+
+url = "https://www.androeed.ru/files/pewdiepies-tuber-simulator.html"
+
+mod_pkg_url = "https://www.androeed.ru/index.php?m=files&f=load_commen_ebu_v_rot_dapda&ui="
+r = requests.get(url)
+host = "https://www.androeed.ru"
+data = r.text
+
+from lxml import etree
+
+from Analysis_data.Xpath_word import Xpaths
+analysis = Xpaths()
+def analysis_data(data):
+    content = etree.HTML(data)
+    data_dic = {}
+    name = content.xpath(analysis.pkg_name)
+    if name and '[' in name[0]:
+        data_dic["name"] = re.search(r'[\d\D]*\[', name[0]).group().replace(' [', "")
+        data_dic["what_news"] = re.search("\[[\d\D]+?\]", name[0]).group().replace('[', '').replace(']', '')
+    elif name:
+        data_dic["name"] = name[0]
+    else:
+        data_dic["name"] = ""
+    data_dic["icon"] = content.xpath(analysis.icon)[0]
+    data_dic["categories"] = ','.join(content.xpath(analysis.categories))
+    data_dic["version"] = content.xpath(analysis.version)[0]
+    data_dic["os"] = content.xpath(analysis.os)[0]
+    data_dic["internet"] = content.xpath(analysis.internet)[0]
+    data_dic["size"] = content.xpath(analysis.size)[0]
+    data_dic["raiting"] = content.xpath(analysis.raiting)[0]
+    data_dic["russian"] = content.xpath(analysis.russian)[0]
+    img_urls = re.findall(r"\('#images_while'\)\.load[\d\D]+?\)", data)
+    if len(img_urls) > 0:
+        try:
+            img_url = img_urls[0].replace("('#images_while').load('", "").replace("\" ')", "").replace("')", "")
+            r = requests.get(url=host + img_url)
+            data = r.text
+            if data:
+                img_content = etree.HTML(data)
+                if img_content.xpath(analysis.img_urls):
+                    data_dic["img_urls"] = ','.join(img_content.xpath(analysis.img_urls))
+            else:
+                print('img_url:' + img_url)
+                data_dic["img_urls"] = "None"
+        except Exception as e:
+            print("error:{},img_urls:{}".format(e, str(img_urls)))
+    else:
+        data_dic["img_urls"] = "None"
+    data_dic["description"] = ''.join(content.xpath(analysis.description))
+    data_dic["app_url"] = content.xpath(analysis.app_url)[0]
+
+    mod_nuber = content.xpath(analysis.mod_number2)
+    if mod_nuber:
+        temp = re.findall("\d+", mod_nuber[-1])
+        if temp:
+            pass
+        else:
+            mod_nuber = content.xpath(analysis.mod_number1)
+    if mod_nuber:
+        temp = re.findall("\d+", mod_nuber[-1])
+        if temp:
+            download_url = temp[-1]
+            print('download_url:' + str(download_url))
+            r = requests.get(url=mod_pkg_url + download_url)
+            data = r.text
+            if data:
+                mod_content = etree.HTML(data)
+                # data_dic["download_first_url"] = mod_content.xpath(analysis.download_first_url)[-1]
+                download_url_len = len(mod_content.xpath(analysis.download_first_url))
+                if download_url_len == 1 or download_url_len == 2:
+                    data_dic["download_first_url"] = [mod_content.xpath(analysis.download_first_url)[-1]]
+                elif download_url_len == 3:
+                    # 第一个为破解包，第二个为apk包
+                    data_dic["download_first_url"] = [mod_content.xpath(analysis.download_first_url)[-1],
+                                                      mod_content.xpath(analysis.download_first_url)[-2]]
+                elif download_url_len == 4:
+                    data_dic["download_first_url"] = [mod_content.xpath(analysis.download_first_url)[-2]]
+                else:
+                    data_dic["download_first_url"] = "None"
+                    print('长度有问题请查看' + data_dic["app_url"])
+            else:
+                data_dic["download_first_url"] = "None"
+        else:
+            data_dic["download_first_url"] = "None"
+            print('is questsion:' + data_dic["app_url"] + ":" + str(mod_nuber) + ',' + str(temp))
+    else:
+        print('没有的url：' + data_dic["app_url"] + str(mod_nuber))
+        data_dic["download_first_url"] = "None"
+    print(data_dic)
+    return data_dic
+
+
+print(analysis_data(data))
