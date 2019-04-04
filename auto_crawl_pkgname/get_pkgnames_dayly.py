f09d7d7b640b8a8aa90b2d0a7447afd8b33f20a4
庄培龙 <peilong.zhuang@office.feng.com>
2019-04-04 12:02:59 +0800

    update

--- auto_crawl_pkgname/get_pkgnames_dayly.py
+++ auto_crawl_pkgname/get_pkgnames_dayly.py
@@ -130,43 +130,47 @@ class CrawlPkgnames:
                 logger.info('download_url:'+str(download_url))
                 data = await self.request_web(url=self.mod_pkg_url+download_url)
                 if data:
-                    mod_content = etree.HTML(data)
-                    # data_dic["download_first_url"] = mod_content.xpath(self.analysis.download_first_url)[-1]
-                    download_url_len = len(mod_content.xpath(self.analysis.download_first_url))
-                    if download_url_len ==1 or download_url_len ==2:
-                        temp_apk_download_url = mod_content.xpath(self.analysis.download_first_url)[-1]
-                        data = await self.request_web(url=temp_apk_download_url)
-                        temp_apk_download_url_data = etree.HTML(data)
-                        apk_download_url = temp_apk_download_url_data.xpath(self.analysis.pkg_download_url)[0]
-                        data_dic["download_first_url"] = [apk_download_url]
-                    elif download_url_len == 3:
-                        # 第一个为破解包，第二个为apk包
-                        temp_apk_download_url = mod_content.xpath(self.analysis.download_first_url)[-2]
-                        data = await self.request_web(url=temp_apk_download_url)
-                        temp_apk_download_url_data = etree.HTML(data)
-                        apk_download_url = temp_apk_download_url_data.xpath(self.analysis.pkg_download_url)[0]
-                        temp_obb_download_url = mod_content.xpath(self.analysis.download_first_url)[-1]
-                        data = await self.request_web(url=temp_obb_download_url)
-                        temp_obb_download_url_data = etree.HTML(data)
-                        obb_download_url = temp_obb_download_url_data.xpath(self.analysis.pkg_download_url)[0]
-                        if '.zip' in obb_download_url:
-                            data_dic["download_first_url"] = [apk_download_url,obb_download_url]
+                    try:
+                        mod_content = etree.HTML(data)
+                        # data_dic["download_first_url"] = mod_content.xpath(self.analysis.download_first_url)[-1]
+                        download_url_len = len(mod_content.xpath(self.analysis.download_first_url))
+                        if download_url_len == 1 or download_url_len == 2:
+                            temp_apk_download_url = mod_content.xpath(self.analysis.download_first_url)[-1]
+                            data = await self.request_web(url=temp_apk_download_url)
+                            temp_apk_download_url_data = etree.HTML(data)
+                            apk_download_url = temp_apk_download_url_data.xpath(self.analysis.pkg_download_url)[0]
+                            data_dic["download_first_url"] = [apk_download_url]
+                        elif download_url_len == 3:
+                            # 第一个为破解包，第二个为apk包
+                            temp_apk_download_url = mod_content.xpath(self.analysis.download_first_url)[-2]
+                            data = await self.request_web(url=temp_apk_download_url)
+                            temp_apk_download_url_data = etree.HTML(data)
+                            apk_download_url = temp_apk_download_url_data.xpath(self.analysis.pkg_download_url)[0]
+                            temp_obb_download_url = mod_content.xpath(self.analysis.download_first_url)[-1]
+                            data = await self.request_web(url=temp_obb_download_url)
+                            temp_obb_download_url_data = etree.HTML(data)
+                            obb_download_url = temp_obb_download_url_data.xpath(self.analysis.pkg_download_url)[0]
+                            if '.zip' in obb_download_url:
+                                data_dic["download_first_url"] = [apk_download_url, obb_download_url]
+                            else:
+                                return None
+                        elif download_url_len == 4:
+                            temp_apk_download_url = mod_content.xpath(self.analysis.download_first_url)[-2]
+                            data = await self.request_web(url=temp_apk_download_url)
+                            temp_apk_download_url_data = etree.HTML(data)
+                            apk_download_url = temp_apk_download_url_data.xpath(self.analysis.pkg_download_url)[0]
+                            data_dic["download_first_url"] = [apk_download_url]
                         else:
-                            return None
-                    elif download_url_len == 4:
-                        temp_apk_download_url = mod_content.xpath(self.analysis.download_first_url)[-2]
-                        data = await self.request_web(url=temp_apk_download_url)
-                        temp_apk_download_url_data = etree.HTML(data)
-                        apk_download_url = temp_apk_download_url_data.xpath(self.analysis.pkg_download_url)[0]
-                        data_dic["download_first_url"] = [apk_download_url]
-                    else:
-                        data_dic["download_first_url"] = "None"
-                        logger.info('长度有问题请查看'+data_dic["app_url"])
+                            data_dic["download_first_url"] = "None"
+                            logger.info('长度有问题请查看' + data_dic["app_url"])
+                    except Exception as e:
+                        logger.info("error:{},url:{}".format(e,data_dic["app_url"]))
+                        return None
                 else:
-                    data_dic["download_first_url"] = "None"
+                    return None
             else:
-                data_dic["download_first_url"] = "None"
-                logger.info('is questsion:'+data_dic["app_url"]+":"+str(mod_nuber)+','+str(temp))
+                logger.info('is questsion:' + data_dic["app_url"] + ":" + str(mod_nuber) + ',' + str(temp))
+                return None
         else:
             logger.info('没有的url：'+ data_dic["app_url"]+str(mod_nuber))
             data_dic["download_first_url"] = "None"
