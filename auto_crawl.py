b5c23508a39b13ab1f51b3a101906697d2877b80
庄培龙 <peilong.zhuang@office.feng.com>
2019-04-09 17:50:29 +0800

    update

--- auto_crawl.py
+++ auto_crawl.py
@@ -1,5 +1,13 @@
+import time
+
+import schedule
+
 from auto_crawl_pkgname.get_pkgnames_dayly import CrawlPkgnames
 
 if __name__ == '__main__':
     t = CrawlPkgnames()
     t.run()
+    schedule.every(2).hours.do(t.run)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
